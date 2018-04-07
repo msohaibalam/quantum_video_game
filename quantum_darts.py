66dc665c1f4d5b8cac3ba4178ee98b956f9de602
M. Sohaib Alam <sohaib.alam@gmail.com>
2018-04-07 14:10:49 -0700

    Interpolating between Hadamard and Identity

--- quantum_darts.py
+++ quantum_darts.py
@@ -1,5 +1,5 @@
 import pygame
-# import numpy as np
+import numpy as np
 import pyquil.api as api
 from pyquil.gates import *
 from pyquil.quil import Program
@@ -27,20 +27,16 @@ size_x = 1000
 size_y = 625
 size = (size_x, size_y)
 # dartboard parameters
-radius_outer = 75
-width = 15
+radius_outer = 60
+width = 10
 radius_1st_inner = radius_outer - 2 * width  # 60
 radius_2nd_inner = radius_outer - 4 * width  # 20
+dartboard_offset = 150
 
 # initialize program with random measurement of either 0 or 1
 p = Program(H(0)).measure(0, [0])
 results_A = qvm.run(p, [0])
 results_B = []
-
-
-# helper function to generate unitary matrix
-def U_(a, b):
-    return np.array([[a, b], [b, -a]])
 
 # initialize pygame
 pygame.init()
@@ -69,13 +65,13 @@ while not game_over:
                     mouse_pos = pygame.mouse.get_pos()
                     print ("Clicked Mouse pos: ", mouse_pos)
                     if (mouse_pos[0] >= size_x//2 - radius_outer) and (mouse_pos[0] <= size_x//2 + radius_outer):
-                        if (mouse_pos[1] >= size_y//2 - 150 - radius_outer) and (mouse_pos[1] <= size_y//2 - 150 + radius_outer):
+                        if (mouse_pos[1] >= size_y//2 - dartboard_offset - radius_outer) and (mouse_pos[1] <= size_y//2 - dartboard_offset + radius_outer):
                             if results_A == [[0]]:
                                 str_text_score = "SCORE!!"
                                 score += 1
                             elif results_A == [[1]]:
                                 str_text_score = "........POOR............"
-                        elif (mouse_pos[1] >= size_y//2 + 150 - radius_outer) and (mouse_pos[1] <= size_y//2 + 150 + radius_outer):
+                        elif (mouse_pos[1] >= size_y//2 + dartboard_offset - radius_outer) and (mouse_pos[1] <= size_y//2 + dartboard_offset + radius_outer):
                             if results_A == [[0]]:
                                 str_text_score = "........POOR............"
                             elif results_A == [[1]]:
@@ -100,28 +96,62 @@ while not game_over:
         # empty out results from dartboard_B
         if len(results_B) > 0:
             results_B.pop()
+        # run the common parts of the program
+        p = Program()
+        if score < 10:
+            a = np.sqrt(1/2.)
+        elif (score >= 10) and (score < 20):
+            a = np.sqrt(3/4.)
+        else:
+            a = np.sqrt(1.)
+        b = np.sqrt(1 - np.square(a))
+        Uf_ = np.array([[a, b], [b, -a]])
+        p.defgate("Uf", Uf_)
         # set dartboard_A center, and calculate results_B in either case
         if results_A == [[0]]:
-            dartboard_A_center = [size_x//2, size_y//2 - 150]
-            p = Program(I(0), H(0)).measure(0, [0])
+            dartboard_A_center = [size_x//2, size_y//2 - dartboard_offset]
+            # run the program for this case
+            p.inst(I(0))
+            p.inst(("Uf", 0))
+            p.measure(0, [0])
             results_B = qvm.run(p, [0])
         elif results_A == [[1]]:
-            dartboard_A_center = [size_x//2, size_y//2 + 150]
-            p = Program(X(0), H(0)).measure(0, [0])
+            dartboard_A_center = [size_x//2, size_y//2 + dartboard_offset]
+            # run the program for this case
+            p.inst(X(0))
+            p.inst(("Uf", 0))
+            p.measure(0, [0])
             results_B = qvm.run(p, [0])
 
     elif color_code % 3 == 1:
         screen.fill(red)
         if len(results_A) > 0:
             results_A.pop()
+        # run the common parts of the program
+        p = Program()
+        if score < 10:
+            a = np.sqrt(1/2.)
+        elif (score >= 10) and (score < 20):
+            a = np.sqrt(3/4.)
+        else:
+            a = np.sqrt(1.)
+        b = np.sqrt(1 - np.square(a))
+        Uf_ = np.array([[a, b], [b, -a]])
+        p.defgate("Uf", Uf_)
         # set dartboard_B centers
         if results_B == [[0]]:
-            dartboard_B_center = [size_x//2 + 150, size_y//2]
-            p = Program(I(0), H(0)).measure(0, [0])
+            dartboard_B_center = [size_x//2 + dartboard_offset, size_y//2]
+            # run the program for this case
+            p.inst(I(0))
+            p.inst(("Uf", 0))
+            p.measure(0, [0])
             results_A = qvm.run(p, [0])
         elif results_B == [[1]]:
-            dartboard_B_center = [size_x//2 - 150, size_y//2]
-            p = Program(X(0), H(0)).measure(0, [0])
+            dartboard_B_center = [size_x//2 - dartboard_offset, size_y//2]
+            # run the program for this case
+            p.inst(X(0))
+            p.inst(("Uf", 0))
+            p.measure(0, [0])
             results_A = qvm.run(p, [0])
 
     elif color_code % 3 == 2:
@@ -150,6 +180,10 @@ while not game_over:
     screen.blit(text_score, [size_x - 150, 20])
     text_attempts = font.render("Attempts: " + str(attempts), True, black)
     screen.blit(text_attempts, [size_x - 150, 40])
+    # text_resultsA = font.render("Results_A: " + str(Results_A), True, black)
+    # screen.blit(text_resultsA, [size_x - 150, 80])
+    # text_resultsB = font.render("Results_A: " + str(Results_B), True, black)
+    # screen.blit(text_resultsA, [size_x - 150, 100])
 
     pygame.display.flip()
 
